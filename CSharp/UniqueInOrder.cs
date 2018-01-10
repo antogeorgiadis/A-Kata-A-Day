416be9a72bff7436d920d11f889c047f2942e9d1
Tony Georgiadis <antogeorgiadis@gmail.com>
2018-01-10 21:02:53 +0200

    OnesAndZeros Kata

---
+++ CSharp/UniqueInOrder.cs
@@ -0,0 +1,29 @@
+/**
+* Level: 6kyu
+* Description: 
+* Implement the function Get() which takes as argument a sequence and returns a list of items without any elements
+* with the same value next to each other and preserving the original order of elements.
+*/
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharp
+{
+  public static class UniqueInOrder
+  {
+    public static IEnumerable<T> Get<T>(IEnumerable<T> iterable) 
+    {
+      var uniqueItemsList = new List<T>();
+      
+      foreach(var element in iterable)
+      {
+        var nextIsDuplicate = element.Equals(uniqueItemsList.LastOrDefault());
+        
+        if (!nextIsDuplicate)
+          uniqueItemsList.Add(element);  
+      }
+
+      return uniqueItemsList;
+    }
+  }
+}
